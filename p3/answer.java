684ef1aad48b0d7b311a402085a7ef0a6d7809f8
0xDarwinC <157836586+0xDarwinC@users.noreply.github.com>
2024-05-17 15:22:45 -0400

    Update answer.java
    Remove the old arraylist implementation

--- p3/answer.java
+++ p3/answer.java
@@ -1,10 +1,6 @@
 import java.util.*;
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        //divide and conquer strategy
-        //int lmax = lengthOfLongestSubstring(s.substring(0, s.length()/2));
-        //int rmax = lengthOfLongestSubstring(s.substring(s.length()/2 + 1));
-
         //Kadane's MCS algo repurposed (probably pretty good because O(n))
         //Keep an array of seen characters and a max
         //if the char never seen, add to sum and keep going.
@@ -15,13 +11,10 @@ class Solution {
         int pointer = 0;
         int preIndex = 0;
         String str = s;
-        //ArrayList<Character> seen = new ArrayList<Character>();
         Hashtable<String, Integer> seen = new Hashtable<String, Integer>();
         while (pointer < str.length()){
-            //preIndex = seen.indexOf(str.substring(pointer, pointer+1).charAt(0));
             //not in our max contig. length
-            if (/*preIndex != -1*/ seen.containsKey(str.substring(pointer, pointer+1)) == false){
-                //seen.add(str.substring(pointer, pointer+1).charAt(0));
+            if (seen.containsKey(str.substring(pointer, pointer+1)) == false){
                 seen.put(str.substring(pointer, pointer+1), pointer);
                 pointer++;
                 sum++;
@@ -29,12 +22,10 @@ class Solution {
             }
             else{
                 //if seen, go to where it was first seen and restart from the letter after.
-                //seen.removeAll(seen);
                 preIndex = seen.get(str.substring(pointer, pointer+1));
                 seen.clear();
                 pointer = preIndex+1;
                 str = str.substring(pointer);
-                //seen.add(str.substring(0, 1).charAt(0));
                 seen.put(str.substring(0, 1), 0);
                 sum = 1;
                 pointer = 1;
